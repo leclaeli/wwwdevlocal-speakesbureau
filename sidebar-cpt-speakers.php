d53605f26a2a848347fdf7d5235e4125d23bd3f0
Elijah LeClair <eleclair@uwm.edu>
2015-02-16 10:02:50 -0600

    added search to sidebar on single speakers pages

--- sidebar-cpt-speakers.php
+++ sidebar-cpt-speakers.php
@@ -1,6 +1,28 @@
 <div id="secondary" class="sidebar-container" role="complementary">
     <div class="widget-area">
         <aside>
+            
+                <h3>Quick Search</h3>
+                <?php get_template_part('searchform-front-page');?>
+                 <ul>
+                    <li><a href="speakers/#topics">Browse Topics</a></li>
+                    <li><a href="speakers/#presentations">Browse Presentations</a></li>
+                </ul>
+                <div class="search-dropdown-container">
+                    <?php $speaker_posts = get_posts('post_type=cpt-speakers&numberposts=-1' ); ?>
+                    <ul>
+                        <?php
+                        foreach ( $speaker_posts as $post ) : setup_postdata( $post ); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </li>
+                        <?php endforeach; 
+                    wp_reset_postdata();?>
+                    </ul>
+                </div>
+            
+        </aside>
+        <aside>
             <h3 id="featured-spk">Most Viewed</h3>
             <ul>
             <?php
